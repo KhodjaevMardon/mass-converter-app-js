c045c28d5c2ac5d71ea7c63a0e00a02f063d52d3
Mardon Khodjaev <zombiee4407@gmail.com>
2023-05-16 20:12:32 +0500

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 let pounds = document.querySelector('#pounds');
 pounds.addEventListener('input', convertToKilos);
+let init_unit = document.querySelector('#initial_unit');
+init_unit.addEventListener('change', convertToKilos);
+let target_unit = document.querySelector('#target_unit');
+target_unit.addEventListener('change', convertToKilos);
+
 
 function convertToKilos() {
-    let kilos = pounds.value * 0.454545455;
-    document.querySelector('#kilograms').textContent = kilos;
+    let initial_multiplier = document.querySelector('#initial_unit').value;
+    let target_multiplier = document.querySelector('#target_unit').value;
+    let kilos = pounds.value * initial_multiplier / target_multiplier;
+    document.querySelector('#target').textContent = kilos;
 }
